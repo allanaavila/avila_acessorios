5399b0f7f477506f20888a2210cc9487f0c8bada
allanaavila <allanac.avila@gmail.com>
2025-02-27 20:04:07 -0300

    criando classes e seus componentes do produto

---
+++ src/main/java/com/avila/acessorios/model/Produto.java
@@ -0,0 +1,36 @@
+package com.avila.acessorios.model;
+
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduto;
+
+    @Column(nullable = false, length = 255)
+    private String nome;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal preco;
+
+    @Column(length = 100)
+    private String material;
+
+    @Column(length = 100)
+    private String categoria;
+
+    @Column(nullable = false)
+    private Integer estoque;
+
+    @Column(name = "dataCadastro", updatable = false)
+    private LocalDateTime dataCadastro = LocalDateTime.now();
+}
