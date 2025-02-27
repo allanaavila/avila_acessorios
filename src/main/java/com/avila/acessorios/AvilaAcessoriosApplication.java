981b6e2604a1367b3b3fec133bdc0b5be60b0872
allanaavila <allanac.avila@gmail.com>
2025-02-27 15:01:12 -0300

    inicializando projeto

---
+++ src/main/java/com/avila/acessorios/AvilaAcessoriosApplication.java
@@ -0,0 +1,13 @@
+package com.avila.acessorios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvilaAcessoriosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvilaAcessoriosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avila/acessorios/AvilaAcessoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avila.acessorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvilaAcessoriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
